835063b3627e2fc6cd58e00bf6fb72a77b925bb0
Thiago Abimeleque <thiagoabimeleque@gmail.com>
2022-11-28 16:53:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTIFdx4Dc0bgMVULPPcUqRfA-a-ZUQ-GkXftTkSNS7R30IOKWismTNNwN-mmt2SVTP5Jynhi3rsEhm0/pub?gid=0&single=true&output=csv', decimal=",")
+df = pd.read_csv('', decimal=",")
 
 df['Total'] = df['Qtde'] * df['Preço']
 
